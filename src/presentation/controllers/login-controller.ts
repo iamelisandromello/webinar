b4e0d74af4523164de8ef571b7b7663c2468ea9a
Elisandro <elisandro@ulbra.edu.br>
2022-03-09 23:54:25 -0300

    feat: add abstration to treatment of errors and exceptions

--- src/presentation/controllers/login-controller.ts
+++ src/presentation/controllers/login-controller.ts
@@ -1,53 +1,13 @@
 import { Controller } from '@/presentation/interfaces/controller'
 import { HttpRequest, HttpResponse } from '@/presentation/interfaces/http'
 import { SignIn } from '@/domain/usecases/user/'
-
-export class MissingParamError extends Error {
-  constructor (paramName: string) {
-    super(`Missing param: ${paramName}`)
-    this.name = 'MissingParamError'
-  }
-}
-
-export class UnauthorizedError extends Error {
-  constructor () {
-    super('Unauthorized')
-    this.name = 'UnauthorizedError'
-  }
-}
-
-export class ServerError extends Error {
-  constructor (error: Error) {
-    super('Internal server error')
-    this.name = 'ServerError'
-    this.stack = error?.stack ?? 'Undefinied Error'
-  }
-}
-
-export const unauthorized = (): HttpResponse => ({
-  statusCode: 401,
-  body: new UnauthorizedError()
-})
-
-export const badRequest = (error: Error): HttpResponse => ({
-  statusCode: 400,
-  body: error
-})
-
-export const success = (data: any): HttpResponse => ({
-  statusCode: 200,
-  body: data
-})
-
-export const serverError = (error: Error): HttpResponse => ({
-  statusCode: 500,
-  body: new ServerError(error) 
-})
+import { badRequest, unauthorized, success, serverError } from '@/presentation/helpers/http'
+import { MissingParamError } from '@/presentation/errors/'
 
 export class LoginController implements Controller {
   constructor (
     private readonly loginService: SignIn
-  ){}
+  ) {}
   async handle (request: HttpRequest): Promise<HttpResponse> {
     try {
       const { user, password} = request.body
